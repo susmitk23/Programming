de36a21eb60581452e4477ae8d7c108f0c231e01
susmitk23 <33984257+susmitk23@users.noreply.github.com>
2017-11-26 19:44:38 +0530

    changes in app.js

--- nodejs/project1/app.js
+++ nodejs/project1/app.js
@@ -8,8 +8,10 @@ fs.readFile('index.html', (err, html) => {
 	if(err){
 	 throw err;
 	}
- 
- const server = http.createServer((req, res) =>{
+
+console.log("Server about to start");
+
+const server = http.createServer((req, res) =>{
 	res.statusCode = 200;
 	res.setHeader('Content-Type' , 'text/html');
 	res.write(html);
